221f56c56f9efdedb2b7c5ddadad621e2d04d123
fasfashann <saintgloria.jkt@gmail.com>
2022-08-08 11:25:49 +0700

    homepage for parabel.id

---
+++ public/front-end/src/js/index.js
@@ -0,0 +1,15 @@
+const buttonLight = document.querySelector(".button-light");
+const buttonDark = document.querySelector(".button-dark");
+
+if (localStorage.getItem("theme") == "dark") setDarkMode(true);
+function setDarkMode(isDark) {
+    if (isDark) {
+        document.body.setAttribute("id", "darkmode");
+        localStorage.setItem("theme", "dark");
+        buttonDark.classList.add("active");
+    } else {
+        document.body.setAttribute("id", "");
+        localStorage.removeItem("theme");
+        buttonDark.classList.remove("active");
+    }
+}
