524616c894a0746213769f2165f166691b80505f
fasfashann <saintgloria.jkt@gmail.com>
2022-08-11 09:37:36 +0700

    add serial page

--- public/front-end/src/js/index.js
+++ public/front-end/src/js/index.js
@@ -1,20 +1,10 @@
-const buttonDark = document.querySelector(".button-dark");
-const svgElementLight = document.getElementById("light");
-const svgElementDark = document.getElementById("moon");
-
 if (localStorage.getItem("theme") == "dark") setDarkMode(true);
 function setDarkMode(isDark) {
     if (isDark) {
         document.body.setAttribute("id", "darkmode");
         localStorage.setItem("theme", "dark");
-        buttonDark.classList.add("active");
-        svgElementLight.style.fill = "#c2c2c2";
-        svgElementDark.style.fill = "black";
     } else {
-        svgElementDark.style.fill = "#c2c2c2";
         document.body.setAttribute("id", "");
         localStorage.removeItem("theme");
-        buttonDark.classList.remove("active");
-        svgElementLight.style.fill = "black";
     }
 }
