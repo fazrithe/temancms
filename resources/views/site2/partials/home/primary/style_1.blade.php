108872414a6d0e006fafa26f16a22ff5a7542b0a
MOH DEDE FAZRI <fazrithe@gmail.com>
2022-08-15 15:16:05 +0700

    update image
    update image

--- resources/views/site2/partials/home/primary/style_1.blade.php
+++ resources/views/site2/partials/home/primary/style_1.blade.php
@@ -27,30 +27,33 @@
                 @endforeach
                 </div>
               </div>
-            <div class="col-lg-6 list-berita-utama">
-                @foreach($blockPosts as $post)
-                <a href="">
-                  <div class=" d-flex">
-                    <a href="{{ route('article.detail', ['id' => $post->slug]) }}">
-                        @if(isFileExist(@$post['image'], $result = @$post['image']->medium_image_three))
-                            <img src="{{ safari_check() ? basePath(@$post['image']).'/'.$result : static_asset('default-image/default-358x215.png')  }} "
-                                 data-original=" {{basePath(@$post['image'])}}/{{ $result }} "
-                                 class="img-fluid" width="80%" alt="{!! $post->title !!}">
-                        @else
-                            <img src="{{ static_asset('default-image/default-358x215.png') }} "
-                                 class="img-fluid" alt="{!! $post->title !!}">
-                        @endif
-                    </a>
-                      <div class="details-news ms-3">
-                      <span class="date"><a href="{{route('article.date', date('Y-m-d', strtotime($post->updated_at)))}}"> {{ $post->updated_at->format('F j, Y') }}</a></span>
-                      <h1  class="title-medium"><a href="{{ route('article.detail', ['id' => $post->slug]) }}">
-                        <p>{!! \Illuminate\Support\Str::limit($post->title, 40) !!}</p></a></h1>
-                    </div>
-                    </div>
-                  </a>
-                  <hr>
-                @endforeach
-              </div>
+              <div class="col-lg-6">
+                    {{--  @php dd($blockPosts); @endphp --}}
+                    @foreach($blockPosts as $post)
+                            <a href="">
+                                <div class=" d-flex">
+                                    <div class="entry-thumbnail col-5">
+                                        <a href="{{ route('article.detail', ['id' => $post->slug]) }}">
+                                            @if(isFileExist(@$post['image'], $result = @$post['image']->medium_image))
+                                                <img src="{{ safari_check() ? basePath(@$post['image']).'/'.$result : static_asset('default-image/default-358x215.png')  }} "
+                                                     data-original=" {{basePath(@$post['image'])}}/{{ $result }} "
+                                                     class="img-fluid" alt="{!! $post->title !!}">
+                                            @else
+                                                <img src="{{ static_asset('default-image/default-358x215.png') }} "
+                                                     class="img-fluid" alt="{!! $post->title !!}">
+                                            @endif
+                                        </a>
+                                    </div>
+                                    <div class="details-news ms-3">
+                                    <span class="date"><a href="{{route('article.date', date('Y-m-d', strtotime($post->updated_at)))}}"> {{ $post->updated_at->format('F j, Y') }}</a></span>
+                                    <h1  class="title-medium"><a href="{{ route('article.detail', ['id' => $post->slug]) }}">
+                                        <p>{!! \Illuminate\Support\Str::limit($post->title, 50) !!}</p></a></h1>
+                                  </div>
+                                  </div>
+                                </a>
+                                <hr>
+                    @endforeach
+            </div>
         </div>
     </div>
 </main>
